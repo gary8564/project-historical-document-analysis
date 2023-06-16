8d739df70b17177fab16e4dc347631ac36481818
Gary <garychang19968564@gmail.com>
2023-06-17 01:36:53 +0200

    attempt2

--- src/utils.py
+++ src/utils.py
@@ -115,7 +115,7 @@ class TexBigDataset(Dataset):
                                        format=datapoints.BoundingBoxFormat.XYXY,
                                        spatial_size=F.get_spatial_size(img),),
                       "labels": torch.zeros(0, dtype=torch.int64),
-                      "image_id": 4,
+                      "image_id": image_id,
                       "area": torch.zeros(0, dtype=torch.float32),
                       "iscrowd": torch.zeros((0,), dtype=torch.int64)}
         # apply the image transforms        
