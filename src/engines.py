d16b8622df7bf7244e3f46fe8350fdbf199ca84b
Gary <garychang19968564@gmail.com>
2023-06-19 13:58:54 +0200

    attempt4

--- src/engines.py
+++ src/engines.py
@@ -142,7 +142,7 @@ def evaluate(model, data_loader, device, gt_annotFilePath):
         outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
         res = {target["image_id"].item(): output for target, output in zip(targets, outputs)}
         img_ids = list(np.unique(list(res.keys())))
-        results, output_filepath = utils.prepare_for_evaluation(res)
+        results, output_filepath = prepare_for_evaluation(res)
         coco_dt = coco_gt.loadRes(output_filepath)
         cocoEval = COCOeval(coco_gt, coco_dt, 'bbox') 
         cocoEval.params.imgIds = img_ids 
